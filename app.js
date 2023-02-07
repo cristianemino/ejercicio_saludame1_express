47e95dbf88987dccf8e79208e4dd5189d0969f21
cristianmino <cristianeisasa@gmail.com>
2023-02-07 17:05:02 -0300

    Resolviendo problemas del ejercicio

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require('body-parser')
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+var persona = "";
+
+app.get('/', function(req, res){
+    if(req.query.nombre){
+        persona = req.query.nombre;
+    }else{
+        persona = "desconcido";
+    }
+    res.send("<h1>Hola "+persona+"!</h1>");
+});
+
+app.listen(3000);
+console.log("Server ok");
